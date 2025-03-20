af462c63982d648a6260cd49d3da1409d7385f84
apolyakov320 <a.polyakov320@gmail.com>
2025-03-20 21:14:50 +0500

    скорректировал ошибки: ввод номера и изменение порядка обработки хэндлеров

--- aiogram_run.py
+++ aiogram_run.py
@@ -3,6 +3,7 @@ import asyncio
 from aiogram import types
 from aiogram.types import BotCommandScopeDefault
 from create_bot import bot, dp, scheduler
+from handlers.keywords import keywords_router
 from handlers.main_menu import start_router 
 from handlers.techsup_menu import techsup_router
 from handlers.request_data import request_router
@@ -10,6 +11,7 @@ from handlers.internet_menu import internet_router
 from handlers.internet_tv_menu import internet_tv_router
 from handlers.internet_sim_menu import internet_sim_router
 from handlers.internet_tv_sim_menu import internet_tv_sim_router
+
 # from work_time.time_func import send_time_msg``
 
 # Команды для личного чата
@@ -25,15 +27,16 @@ async def set_private_commands():
 async def main(): 
     # scheduler.add_job(send_time_msg, 'interval', seconds=10) # добавляем задачу в планировщик scheduler
     # scheduler.start() # запускаем планировщик задач, чтобы он начал выполнять добавленные задачи по расписанию
-    # Удаление команд для групп
     # добавляем роутеры в диспетчер dp
     dp.include_router(start_router) 
+    dp.include_router(keywords_router)
     dp.include_router(techsup_router)
     dp.include_router(request_router) 
     dp.include_router(internet_router) 
     dp.include_router(internet_tv_router) 
     dp.include_router(internet_sim_router) 
     dp.include_router(internet_tv_sim_router)
+    
 # запуск бота в режиме long polling при запуске бот очищает все обновления, которые были за его моменты бездействия    
     try:
         await bot.delete_webhook(drop_pending_updates=True) # пропустить накопленные апдейты
---
+++ handlers/keywords.py
@@ -0,0 +1,19 @@
+from aiogram import Router, F # роутер и "магический фильтр"
+from aiogram.types import Message
+from create_bot import bot, admins
+
+
+keywords_router = Router()
+# Хэндлер для обработки ключевых слов
+@keywords_router.message(F.text)
+async def handle_keywords(message: Message):
+    KEYWORDS = ['ростелеком', 'интернет', 'связь', 'приставка']
+    if any(keyword in message.text.lower() for keyword in KEYWORDS):
+        await message.answer("Мы заметили, что у вас возникла проблема. Обратитесь в техническую поддержку или ожидайте, пока с вами свяжется администратор.")
+        for admin_id in admins:
+            await bot.send_message(
+                admin_id,
+                f"⚠️ Пользователь @{message.from_user.username} (ID: {message.from_user.id}) написал сообщение с ключевым словом:\n\n"
+                f"<b>Текст сообщения:</b>\n{message.text}",
+                parse_mode="HTML"
+            )
