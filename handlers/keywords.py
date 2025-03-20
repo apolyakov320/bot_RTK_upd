793ef421fd3e79d8f29fdc444a748d67091be817
apolyakov320 <a.polyakov320@gmail.com>
2025-03-20 22:24:32 +0500

    добавил метод lower() для определения ключевых слов в любом регистре

--- handlers/keywords.py
+++ handlers/keywords.py
@@ -5,7 +5,7 @@ from create_bot import bot, admins
 
 keywords_router = Router()
 # Хэндлер для обработки ключевых слов
-@keywords_router.message(F.text.contains("ростелеком") | F.text.contains("интернет")| F.text.contains("связь")| F.text.contains("приставка"))
+@keywords_router.message(F.text.lower().contains("ростелеком") | F.text.lower().contains("интернет")| F.text.lower().contains("связь")| F.text.contains("приставка"))
 async def handle_keywords(message: Message):
         await message.answer("Мы заметили, что у вас возникла проблема. Обратитесь в техническую поддержку или ожидайте, пока с вами свяжется администратор.")
         for admin_id in admins:
