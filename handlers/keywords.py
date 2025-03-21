ea584763c6936cbe31026b3b19384bdc038dc46f
apolyakov320 <a.polyakov320@gmail.com>
2025-03-21 15:54:36 +0500

    добавлены ключевые слова для определения и реакции

--- handlers/keywords.py
+++ handlers/keywords.py
@@ -5,7 +5,10 @@ from create_bot import bot, admins
 
 keywords_router = Router()
 # Хэндлер для обработки ключевых слов
-@keywords_router.message(F.text.lower().contains("ростелеком") | F.text.lower().contains("интернет")| F.text.lower().contains("связь")| F.text.contains("приставка"))
+@keywords_router.message(F.text.lower().contains("ростелеком") | F.text.lower().contains("интернет")| F.text.lower().contains("связь")| F.text.lower().contains("приставка")|
+                         F.text.lower().contains("инет")| F.text.lower().contains("телевидение")| F.text.lower().contains("телевизор")| F.text.lower().contains("мтс")|
+                         F.text.lower().contains("дом.ру")| F.text.lower().contains("билайн")| F.text.lower().contains("русская компания")| F.text.lower().contains("телесети")|
+                         F.text.lower().contains("оператор")| F.text.lower().contains("провайдер")| F.text.lower().contains("ростелек")| F.text.lower().contains("телек"))
 async def handle_keywords(message: Message):
         await message.answer("Мы заметили, что у вас возникла проблема. Обратитесь в техническую поддержку или ожидайте, пока с вами свяжется администратор.")
         for admin_id in admins:
